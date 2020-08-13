08c0f2cf575132a5f541b3efaa28436326dfcf27
Mayank Raj <mayank227raj@gmail.com>
2020-08-14 02:39:02 +0530

    Model with validtion code

--- Stress_model/sirius.py
+++ Stress_model/sirius.py
@@ -1,11 +1,13 @@
 # %% [code]
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 import time
 import tensorflow as tf
 from tensorflow.keras import Model, layers, optimizers, losses
 from tensorflow import keras
 from tensorflow.keras.utils import Sequence, plot_model
+from tensorflow.keras.models import load_model
 import math
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
@@ -25,6 +27,9 @@ NUM_WORKERS = 10
 NEIGHBORS = 4
 LOCAL_LEN = 2*NEIGHBORS + 1
 
+# Train test split of the element ids
+train_set, test_set = train_test_split(phase_dist.index.values, test_size=0.25)
+
 tf.keras.backend.set_floatx('float64')
 config = tf.compat.v1.ConfigProto(inter_op_parallelism_threads=12,
                                   device_count={'CPU': 20},
@@ -39,6 +44,7 @@ class Dataset(Sequence):
                  phase_dist,
                  element_arrangement,
                  element_index,
+                 subset,
                  mean=mean,
                  var=variance,
                  neighbors=NEIGHBORS,
@@ -51,14 +57,15 @@ class Dataset(Sequence):
         self.mean = mean
         self.std = var**0.5
         self.neighbors = neighbors
+        self.subset = subset
 
     def __len__(self):
-        return math.ceil(len(self.phase_dist) / self.batch_size)
+        return math.ceil(len(self.subset) / self.batch_size)
 
     def __getitem__(self, index):
-        element_ids = self.phase_dist.index[index *
-                                            self.batch_size:(index + 1)
-                                            * self.batch_size].values
+        element_ids = self.subset[index *
+                                  self.batch_size:(index + 1)
+                                  * self.batch_size]
         target = []
         local_phase_dist = []
         for element_id in element_ids:
@@ -136,29 +143,39 @@ x = layers.Dense(50, activation=keras.activations.tanh,
 output = layers.Dense(1, name='output')(x)
 model = Sirius(inputs=fixed_input, outputs=output)
 
+train_dataset = Dataset(frame_description,
+                        phase_dist, element_arrangement,
+                        element_index,
+                        train_set,
+                        batch_size=BATCH_SIZE)
 
-# plot_model(model,to_file='model_plot.png',show_shapes=True,expand_nested=True)
-
-
-dataset = Dataset(frame_description,
-                  phase_dist, element_arrangement,
-                  element_index,
-                  batch_size=BATCH_SIZE)
+test_dataset = Dataset(frame_description,
+                       phase_dist, element_arrangement,
+                       element_index,
+                       test_set,
+                       batch_size=BATCH_SIZE)
 
 model.compile(loss=losses.MeanSquaredError(),
               optimizer=optimizers.Adam(learning_rate=1E-3),
-              metrics=[tf.keras.metrics.MeanSquaredError()])
+              metrics=[tf.keras.metrics.MeanAbsoluteError()])
+
+callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss',
+                                           min_delta=0.0001,
+                                           patience=10)]
 
 tic = time.process_time()
-logs = model.fit(dataset, epochs=EPOCHS, workers=NUM_WORKERS, use_multiprocessing=True)
+logs = model.fit(train_dataset,
+                 validation_data=test_dataset,
+                 callbacks=callbacks,
+                 epochs=EPOCHS,
+                 workers=NUM_WORKERS,
+                 use_multiprocessing=True)
 toc = time.process_time()
 print('Process time (seconds):\t', toc-tic)
 
 # Save logs
 for key in logs.history.keys():
     np.save(key, logs.history[key])
-# Prediction
-result = model.predict(dataset, workers=NUM_WORKERS, use_multiprocessing=True)
-np.save('predicted_stress', result)
+
 # Save model
 model.save('model')
